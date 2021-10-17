faa74836388c56db5c19e9f5696dc704dc1773e6
kgfathur <sahabatsolder@gmail.com>
2021-10-17 23:59:55 +0700

    initial commit

---
+++ sample-web/redeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Destroy the services..."
+docker-compose -f docker-compose.yaml down -v
+sleep 3
+echo "Bring the services back..."
+docker-compose -f docker-compose.yaml up -d
+echo ""
+sleep 10
+echo "Install PHP Extension"
+docker exec -it webapp1 /usr/local/bin/docker-php-ext-install mysqli
+docker restart webapp1
