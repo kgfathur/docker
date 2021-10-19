4683f4b3061d7696214159299e19398df0113224
kgfathur <sahabatsolder@gmail.com>
2021-10-20 01:08:57 +0700

    modify template for minimal version

--- sample-web/deploy-test.sh
+++ sample-web/deploy-test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-test.yaml up -d
-echo ""
-sleep 10
-echo "Install PHP Extension"
-docker exec -it webapp1 /usr/local/bin/docker-php-ext-install mysqli
-docker restart webapp1
