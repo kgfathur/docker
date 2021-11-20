63c3f0cfcf090e35851b9cb4e9f82c0c897fd756
kgfathur <sahabatsolder@gmail.com>
2021-11-20 20:14:09 +0700

    add mariadb-galera

---
+++ mariadb-galera/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# synchronize template
+../rsync-src.sh
+
+export $(cat .env) > /dev/null 2>&1;
+echo ""
+echo -e "Stack Name\t= ${1:-$STACK_NAME}";
+echo -e "Image Version \t= $VERSION"
+echo -e "MANAGER1 \t= $MANAGER1"
+echo -e "MANAGER2 \t= $MANAGER2"
+echo -e "MANAGER3 \t= $MANAGER3"
+echo -e "WORKER1 \t= $WORKER1"
+echo -e "WORKER2 \t= $WORKER2"
+echo ""
+
+docker stack deploy -c stack.yml ${STACK_NAME}
