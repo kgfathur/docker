06c2d557f900e11633561a11233dbbc60750cb57
kgfathur <sahabatsolder@gmail.com>
2022-02-13 16:37:51 +0700

    update deploy.sh

--- mariadb-galera/deploy.sh
+++ mariadb-galera/deploy.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+mode=$1
+
 # synchronize template
-../rsync-src.sh
+# ../rsync-src.sh
+if [[ -z "$mode" || "$mode" == "compose" ]]; then
+  echo "Using docker compose..."
+  docker-compose -f docker-compose.yaml -p galera up -d
 
-export $(cat .env) > /dev/null 2>&1;
-echo ""
-echo -e "Stack Name\t= ${1:-$STACK_NAME}";
-echo -e "Image Version \t= $VERSION"
-echo -e "MANAGER1 \t= $MANAGER1"
-echo -e "MANAGER2 \t= $MANAGER2"
-echo -e "MANAGER3 \t= $MANAGER3"
-echo -e "WORKER1 \t= $WORKER1"
-echo -e "WORKER2 \t= $WORKER2"
-echo ""
+elif [[ "$mode" == "swarm" ]]; then
+  echo "Using docker swarm..."
+  export $(cat .env) > /dev/null 2>&1;
+  echo ""
+  echo -e "Stack Name\t= ${1:-$STACK_NAME}";
+  echo -e "Image Version \t= $VERSION"
+  echo -e "MANAGER1 \t= $MANAGER1"
+  echo -e "MANAGER2 \t= $MANAGER2"
+  echo -e "MANAGER3 \t= $MANAGER3"
+  echo -e "WORKER1 \t= $WORKER1"
+  echo -e "WORKER2 \t= $WORKER2"
+  echo ""
 
-docker stack deploy -c stack.yml ${STACK_NAME}
+  docker stack deploy -c stack.yml ${STACK_NAME}
+fi
--- mariadb-galera/destroy.sh
+++ mariadb-galera/destroy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-export $(cat .env) > /dev/null 2>&1;
-echo -e "Stack Name\t= ${1:-$STACK_NAME}";
-echo ""
-docker stack rm ${1:-$STACK_NAME}
+mode=$1
+
+if [[ -z "$mode" || "$mode" == "compose" ]]; then
+  echo "Using docker compose..."
+  docker-compose -f docker-compose.yaml -p galera down -v
+
+elif [[ "$mode" == "swarm" ]]; then
+  export $(cat .env) > /dev/null 2>&1;
+  echo -e "Stack Name\t= ${1:-$STACK_NAME}";
+  echo ""
+  docker stack rm ${1:-$STACK_NAME}
+fi
